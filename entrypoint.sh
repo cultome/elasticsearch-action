68adb77a9e3d21e023ee8924ed7f84534d22fe1b
Carlos Soria <csoria@cultome.io>
2022-01-10 18:20:12 -0600

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 docker network create elastic
 
 docker_run="docker run"
-docker_run="$docker_run --network elastic -e 'node.name=es1' -e 'cluster.name=docker-elasticsearch' -e 'cluster.routing.allocation.disk.threshold_enabled=false' -e 'bootstrap.memory_lock=true' -e 'ES_JAVA_OPTS=-Xms1g -Xmx1g' --ulimit nofile=65536:65536 --ulimit memlock=-1:-1 --name='es1' -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT -e discovery_type=$INPUT_DISCOVERY_TYPE docker.elastic.co/elasticsearch/elasticsearch-oss::$INPUT_ELASTICSEARCH_VERSION"
+docker_run="$docker_run --network elastic -e 'node.name=es1' -e 'cluster.name=docker-elasticsearch' -e 'cluster.routing.allocation.disk.threshold_enabled=false' -e 'bootstrap.memory_lock=true' -e 'ES_JAVA_OPTS=-Xms1g -Xmx1g' --ulimit nofile=65536:65536 --ulimit memlock=-1:-1 --name='es1' -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT -e discovery_type=$INPUT_DISCOVERY_TYPE docker.elastic.co/elasticsearch/elasticsearch-oss:$INPUT_ELASTICSEARCH_VERSION"
 
 sh -c "$docker_run"
 
-docker run --network elastic --rm appropriate/curl --max-time 120 --retry 120 --retry-delay 1 --retry-connrefused --show-error --silent http://es1:9200
+docker run --network elastic --rm appropriate/curl --max-time 20 --retry 10 --retry-delay 1 --retry-connrefused --show-error --silent http://es1:9200
