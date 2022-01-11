d1b9d34f01969adbf83c17c98f59e008cdd5edc4
Carlos Soria <csoria@cultome.io>
2022-01-10 18:03:54 -0600

    Make changes to work with version 6.3.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,8 @@
 
 docker network create elastic
 
-
 docker_run="docker run"
-docker_run="$docker_run --network elastic -e 'node.name=es1' -e 'cluster.name=docker-elasticsearch' -e 'cluster.initial_master_nodes=es1' -e 'discovery.seed_hosts=es1' -e 'cluster.routing.allocation.disk.threshold_enabled=false' -e 'bootstrap.memory_lock=true' -e 'ES_JAVA_OPTS=-Xms1g -Xmx1g' -e 'xpack.security.enabled=false' -e 'xpack.license.self_generated.type=basic' --ulimit nofile=65536:65536 --ulimit memlock=-1:-1 --name='es1' -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT -p $INPUT_HOST_NODE_PORT:$INPUT_NODE_PORT -e discovery_type=$INPUT_DISCOVERY_TYPE elasticsearch:$INPUT_ELASTICSEARCH_VERSION"
+docker_run="$docker_run --network elastic -e 'node.name=es1' -e 'cluster.name=docker-elasticsearch' -e 'cluster.routing.allocation.disk.threshold_enabled=false' -e 'bootstrap.memory_lock=true' -e 'ES_JAVA_OPTS=-Xms1g -Xmx1g' --ulimit nofile=65536:65536 --ulimit memlock=-1:-1 --name='es1' -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT -e discovery_type="single-node" docker.elastic.co/elasticsearch/elasticsearch-oss::$INPUT_ELASTICSEARCH_VERSION"
 
 sh -c "$docker_run"
 
